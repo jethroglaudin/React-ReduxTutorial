351c9cdf0342512d11f210a9d3958bee792dca4e
Jethro Glaudin <jethroglaudin.student@careerdevs.com>
2019-05-09 15:42:14 -0400

    poketimes added

---
+++ poketimes/src/components/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const Contact = () => {
+    return (
+        <div className="container">
+            <h4 className="center">Contact</h4>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam sint, quaerat tenetur voluptatum perspiciatis praesentium ipsum error. Quod sequi aliquid reprehenderit perspiciatis laudantium temporibus, pariatur dignissimos provident ratione eius placeat.</p>
+        </div>
+    )
+};
+
+export default Contact;
---
+++ poketimes/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const Home = () => {
+    return (
+        <div className="container">
+            <h4 className="center">Home</h4>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam sint, quaerat tenetur voluptatum perspiciatis praesentium ipsum error. Quod sequi aliquid reprehenderit perspiciatis laudantium temporibus, pariatur dignissimos provident ratione eius placeat.</p>
+        </div>
+    )
+};
+
+export default Home;
