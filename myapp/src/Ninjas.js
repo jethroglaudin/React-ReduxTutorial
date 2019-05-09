a51c1a4eda21bb6e1ff9556cf8f0d4663c7a6887
Jethro Glaudin <jethroglaudin.student@careerdevs.com>
2019-05-08 21:28:01 -0400

    lesson16 - Conditional Output

--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -2,27 +2,53 @@ import React, { Component } from 'react';
 
 //functional component 
 // don't need the state anymore
- 
-// you could also destructure right inside the () with ({})
-const Ninjas = ({ninjas}) =>  {
-        //destructuring or store the props in a variable
-        // const { ninjas } = props;
-        const ninjaList = ninjas.map(ninja => {
-            return (
-                <div className ="ninja" key={ninja.id}> 
-                <div>Name: { ninja.name }</div>
-                <div>Age: { ninja.age }</div>
-                <div>Belt: { ninja.belt }</div>
-            </div>
 
-            )
-        })
-        return (
-          <div className="ninja-list">
-              { ninjaList }
-          </div>
-        )
-    
+// you could also destructure right inside the () with ({})
+const Ninjas = ({ ninjas }) => {
+    //destructuring or store the props in a variable
+    // const { ninjas } = props;
+    // const ninjaList = ninjas.map(ninja => {
+    //     if (ninja.age > 20){
+    //     return (
+    //         <div className ="ninja" key={ninja.id}> 
+    //         <div>Name: { ninja.name }</div>
+    //         <div>Age: { ninja.age }</div>
+    //         <div>Belt: { ninja.belt }</div>
+    //     </div>
+
+    //     )
+    //     } else {
+    //         return null
+    //     }
+    // })
+    // const ninjaList = ninjas.map(ninja => {
+    //     //ternary operator condition ? then first return value () : if true then the second () if false
+    //     return ninja.age > 20 ? (
+    //         <div className="ninja" key={ninja.id}>
+    //             <div>Name: {ninja.name}</div>
+    //             <div>Age: {ninja.age}</div>
+    //             <div>Belt: {ninja.belt}</div>
+    //         </div>
+    //     ) : null;
+    // })
+    return (
+        <div className="ninja-list">
+            {/* you may also place the logic inside the template */}
+            {
+                ninjas.map(ninja => {
+                    //ternary operator condition ? then first return value () : if true then the second () if false
+                    return ninja.age > 20 ? (
+                        <div className="ninja" key={ninja.id}>
+                            <div>Name: {ninja.name}</div>
+                            <div>Age: {ninja.age}</div>
+                            <div>Belt: {ninja.belt}</div>
+                        </div>
+                    ) : null;
+                })
+            }
+        </div>
+    )
+
 }
 
 export default Ninjas
