4244fdfaab341ca09c23c9adbcd8e182485208b6
Jethro Glaudin <jethroglaudin.student@careerdevs.com>
2019-05-10 16:18:06 -0400

    lesson28 - Higher Order Components

--- poketimes/src/components/About.js
+++ poketimes/src/components/About.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import Rainbow from './HigherOrderComponent/Rainbow';
 
 const About = () => {
     return (
@@ -7,7 +7,7 @@ const About = () => {
             <h4 className="center">About</h4>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam sint, quaerat tenetur voluptatum perspiciatis praesentium ipsum error. Quod sequi aliquid reprehenderit perspiciatis laudantium temporibus, pariatur dignissimos provident ratione eius placeat.</p>
         </div>
-    )
+    );
 };
 
-export default About;
+export default Rainbow(About);
